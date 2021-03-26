214003363f47aa1cf67340e289ebadddcd60dd4b
kizilejder <emrahdogan93@hotmail.com>
2021-03-26 17:32:29 +0300

    Initial commit

---
+++ Deneme/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product k = new Product();
+            Product k1 = new Product();
+
+            k.Isim = "Hobbit";
+            k.Tur = "Roman";
+            k.Yazar = "J.R.R.Tolkien";
+
+            k1.Isim = "Harry Potter";
+            k1.Tur = "Roman";
+            k1.Yazar = "J.K. Rowling";
+
+            Product[] books = new Product[] { k, k1 };
+            //for each kısmı
+
+            foreach (var dizi in books)
+            {
+                Console.WriteLine(" " + dizi.Isim + " " + dizi.Tur + " " + dizi.Yazar);
+            }
+            //for bölümü
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine(" " + books[i].Isim + " " + books[i].Tur + " " + books[i].Yazar);
+            }
+
+            int j = 0;
+            while (j != books.Length)
+            {
+                Console.WriteLine(" " + books[j].Isim + " " + books[j].Tur + " " + books[j].Yazar);
+                j++;
+            }
+        }
+
+        class Product
+        {
+            public string Isim { get; set; }
+            public string Tur { get; set; }
+            public string Yazar { get; set; }
+
+        }
+
+    }
+}
